8388982c5b0dc14f252971c76bb5df84b923e2bd
Enzo Barlotta <ebarlotta@yahoo.com.ar>
2022-09-02 20:50:52 -0300

    cuarto

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,10 +16,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call(RubroSeeder::class);
         $this->call(TipoivaSeeder::class);
-<<<<<<< HEAD
         $this->call(TiposuserSeeder::class);
-=======
         $this->call(TipouserSeeder::class);
->>>>>>> fe69eff6c2065fcfaf6699d311ef8f7ef9023106
     }
 }
